4304114f365efd700d5293f19fc28d49827f5c06
Andre <quadhay@gmail.com>
2022-01-06 20:33:30 +0100

    saved

--- src/component/SecondHeader/index.js
+++ src/component/SecondHeader/index.js
@@ -42,7 +42,7 @@ const Index=()=>{
     },[])
     return(
         <div className={scroll >= 583 ? 'headerScroll':'Displaynone'}>
-                <div className="logo" style={{cursor:'pointer'}}><Link to='/'><img src={Squirreling} onClick={()=>{window.scroll(0,0)}}/></Link></div>
+                <div className="logoII" style={{cursor:'pointer'}}><Link to='/'><img src={Squirreling} onClick={()=>{window.scroll(0,0)}}/></Link></div>
                 <div className='nav-bars-scroll'>
                     
                     <Link  to="aboutus" spy={true} smooth={true}>
