b00d0189a1169fc1c5abdc6627d16e8826e0fcc9
Andre <quadhay@gmail.com>
2022-01-06 22:48:11 +0100

    saved

--- src/layout/Home/Roadmap/index.js
+++ src/layout/Home/Roadmap/index.js
@@ -34,21 +34,101 @@ const Index = ()=>{
             <div class="roadmapStyle mt-4">
                 <div class="row">
                     <div class="col-md-12 styledRoadmap">
-                        <div class="timerliner-div">
-                            <ul className='timeliner'>
-                                <li className='q1'>Q1 2022</li>
-                                <li className='q2'>Q1 2022</li>
-                                <li className='q3'>Q2 2022</li>
-                                <li className='q3'>Q3 2022</li>
-                                {/* <li>Q2</li>
-                                <li>Q3</li>
-                                <li>Q14</li> */}
-                            </ul>
+                        <div className='coverDiv'>
+                            <div className="newtimeline">
+                                <div>Q1 2022</div>
+                                <ul className='me'>
+                                    <li>
+                               
+                                        <div className='roadmapComponent'>
+                                            <div className='roadmap-flex-1'>
+                                            <div className='roadmapComponent-title'><h6>Sold out, Metaverse here we come!</h6></div>
+                                                <div className='roadmapComponent-text'>
+                                                    <p>
+                                                        After selling all 5,500NFTs, our journey on the Metaverse starts. As an appreciation to SS community, we will have many gifts during and after the sales, stay tuned!
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className='roadmap-flex-2'>
+                                                <img src={item1}/>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+        
+                            </div>
+                            <div className="newtimeline">
+                                <div>Q1 2022</div>
+                                <ul className='me'>
+                                    <li>
+                                        <div className='roadmapComponent'>
+                                            <div className='roadmap-flex-1'>
+                                            <div className='roadmapComponent-title'><h6>Acquiring a land on Sandbox!</h6></div>
+                                                <div className='roadmapComponent-text'>
+                                                    <p>
+                                                    Acquire a Land on Sandbox, to build our Squirreling Squirrels Community, where each holder has access to join and have fun. The land will be open to all holders for them to experience a new and beautiful adventure on the metaverse full of surprises! Key access to social & gaming events, art galleries, and more! Join our Discord to know more of what is coming
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className='roadmap-flex-2'>
+                                                <img src={item4}/>
+                                            </div>
+                                        </div>
+                                     </li>
+                                </ul>
+        
+                            </div>
+                            <div className="newtimeline">
+                                <div>Q2 2022</div>
+                                <ul className='me'>
+                                    <li>
+                                        <div className='roadmapComponent'>
+                                            <div className='roadmap-flex-1'>
+                                            <div className='roadmapComponent-title'><h6>Making the world a better place, tree by tree</h6></div>
+                                                <div className='roadmapComponent-text'>
+                                                    <p>
+                                                    A million starts with one, that one starts with each holder of our SS collection. As part of our commitment to help the global community, we will be planting 100,000 trees. And as a thank you to our community, each holder will get a certification in their name for the trees that have been planted. More details to be revealed soon.
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className='roadmap-flex-2'>
+                                                <img src={item3}/>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+        
+                            </div>
+                            <div className="newtimeline">
+                                <div>Q3 2022</div>
+                                <ul className='me'>
+                                    <li>
+                                    
+                                    
+                                        <div className='roadmapComponent'>
+                                            <div className='roadmap-flex-1'>
+                                            <div className='roadmapComponent-title'><h6>Squirreling Squirrels Game</h6></div>
+                                                <div className='roadmapComponent-text'>
+                                                    <p>
+                                                    Our goal is to develop a game on our metaverse land. Development stages are ongoing, and all our SS community will be able to join in on the fun. Nevertheless, SS NFT holders will have more benefits with their unique characters and more.  The game will consist of different levels where players will progress by completing certain games, tasks and challenges to find the acorn. It will be a play to earn game for all holders as well!
+                                                    </p>
+                                                </div>
+                                            </div>
+                                            <div className='roadmap-flex-2'>
+                                                <img src={item2}/>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+        
+                            </div>
                         </div>
                         
-                        <ul class="timeline">
+
+                        {/* <ul class="timeline">
                         
                             <li>
+                               
                                     <div className='roadmapComponent'>
                                         <div className='roadmap-flex-1'>
                                         <div className='roadmapComponent-title'><h6>Sold out, Metaverse here we come!</h6></div>
@@ -112,7 +192,7 @@ const Index = ()=>{
                                         </div>
                                     </div>
                             </li>
-                        </ul>
+                        </ul> */}
                     </div>
                 </div>
             </div>
