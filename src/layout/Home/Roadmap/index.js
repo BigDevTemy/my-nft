1ae95501b3fd3730d6b0395d2dd712909fd4aa21
Andre <quadhay@gmail.com>
2022-01-10 15:57:49 +0100

    Saved

--- src/layout/Home/Roadmap/index.js
+++ src/layout/Home/Roadmap/index.js
@@ -45,7 +45,7 @@ const Index = ()=>{
                                             <div className='roadmapComponent-title'><h6>Sold out, Metaverse here we come!</h6></div>
                                                 <div className='roadmapComponent-text'>
                                                     <p>
-                                                        After selling all 5,500NFTs, our journey on the Metaverse starts. As an appreciation to SS community, we will have many gifts during and after the sales, stay tuned!
+                                                        After selling out, our journey on the Metaverse starts. As an appreciation to SS community, we will have many gifts during and after the sales, stay tuned!
                                                     </p>
                                                 </div>
                                             </div>
@@ -66,7 +66,7 @@ const Index = ()=>{
                                             <div className='roadmapComponent-title'><h6>Acquiring a land on Sandbox!</h6></div>
                                                 <div className='roadmapComponent-text'>
                                                     <p>
-                                                    Acquire a Land on Sandbox, to build our Squirreling Squirrels Community, where each holder has access to join and have fun. The land will be open to all holders for them to experience a new and beautiful adventure on the metaverse full of surprises! Key access to social & gaming events, art galleries, and more! Join our Discord to know more of what is coming
+                                                    Acquire  Land on Sandbox, to build our Squirreling Squirrels Community, where each holder has access to join and have fun. The land will be open to all holders for them to experience a new and beautiful adventure on the metaverse full of surprises! Key access to social & gaming events, art galleries, and more! Join our Discord to know more of what is coming
                                                     </p>
                                                 </div>
                                             </div>
@@ -87,7 +87,7 @@ const Index = ()=>{
                                             <div className='roadmapComponent-title'><h6>Making the world a better place, tree by tree</h6></div>
                                                 <div className='roadmapComponent-text'>
                                                     <p>
-                                                    A million starts with one, that one starts with each holder of our SS collection. As part of our commitment to help the global community, we will be planting 100,000 trees. And as a thank you to our community, each holder will get a certification in their name for the trees that have been planted. More details to be revealed soon.
+                                                    A million starts with one, that one starts with each holder of our SS collection. As part of our commitment to help the global community, we will by planting 100,000 trees. And as a thank you to our community, each holder will get a certification in their name for the trees that have been planted. More details to be revealed soon.
                                                     </p>
                                                 </div>
                                             </div>
