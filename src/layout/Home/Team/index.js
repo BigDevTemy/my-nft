5b0771778c8b646fe31bedbf0c0e2c8faa10d78d
Andre <quadhay@gmail.com>
2022-01-18 16:57:25 +0100

    Saved

--- src/layout/Home/Team/index.js
+++ src/layout/Home/Team/index.js
@@ -25,181 +25,117 @@ const Index=()=>{
 
             </div>
 
-{/*              
-                <div className="team-pic-div">
-                
-                    <div className="team-1">
-                        <img src={Marilyn} className="char-img"/>
-                        <div className="jobTitle">
-                            <span className="official-title">CEO</span>
-                            <span className="officer-name">Clark</span>
-                            <span className="job-description">Design & Operation</span>
-                        </div>
-                    </div>
-                    <div className="team-2">
-                        <img src={team1} className="char-img"/>
-                        <div className="jobTitle">
-                                <span className="official-title">CMO</span>
-                                <span className="officer-name">Kaylee</span>
-                                <span className="job-description">Community & Marketing</span>
-                        </div>
-                    </div>
-                    <div className="team-3">
-                        <img src={Anthony} className="char-img"/>
-                        <div className="jobTitle">
-                            <span className="official-title">CTO</span>
-                            <span className="officer-name">Jack</span>
-                            <span className="job-description">Technology & Blockchain</span>
-                        </div>
-
-                    </div>
-                   
-                     
-
-                    
-                    
-                    
-                </div> */}
-                {/* <div className="team-pic-div-2">
-                <div className="team-5">
-                        <img src={team1} className="char-img"/>
-                        <div className="jobTitle">
-                            <span className="official-title">Software Engineer</span>
-                            <span className="officer-name">Marilyn</span>
-                            <span className="job-description">Smart Contract/Developer</span>
-                        </div>
-
-                    </div>
-                    <div className="team-5">
-                        <img src={team1} className="char-img"/>
-                        <div className="jobTitle">
-                            <span className="official-title">Architect/Artist</span>
-                            <span className="officer-name">Chris</span>
-                            <span className="job-description">Designer</span>
-                        </div>
-
-                    </div>
-                    <div className="team-6">
-                        <img src={Anthony} className="char-img"/>
-                        <div className="jobTitle">
-                            <span className="official-title">Game Marker</span>
-                            <span className="officer-name">Anthony</span>
-                            <span className="job-description">Architect/Artist</span>
-                        </div>
-
-                    </div>
-                </div> */}
-
-                <div className="teamAll">
-                    <div className="team-member">
-                        <div className="teamImg">
-                            <img src={Ceekay}/>
-                        </div>
-                        <div className='teamInfor'>
-                            <div className='mytitle'>
-                                Ceekay
-                            </div>
-                            <div className='teamposition'>
-                                {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
-                                CEO <br/> Operations
-                            </div>
-                            <div className='twitterIcon'>
-                                {/* <div className='twitter'>
-                                    <FiTwitter size={20} color='#fff' />
-                                </div> */}
-                               
-                            </div>
-                        </div>
-                    </div>
-                    <div className="team-member">
+            <div className="teamAll">
+                <div className="team-member">
                     <div className="teamImg">
-                            <img src={Karim}/>
+                        <img src={Ceekay}/>
+                    </div>
+                    <div className='teamInfor'>
+                        <div className='mytitle'>
+                            Ceekay
                         </div>
-                        <div className='teamInfor'>
-                            <div className='mytitle'>
-                                Karim
-                            </div>
-                            <div className='teamposition'>
-                                {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
-                                CMO <br/> Marketing
-                            </div>
-                            <div className='twitterIcon'>
-                                {/* <div className='twitter'>
-                                    <FiTwitter size={20} color='#fff' />
-                                </div> */}
-                               
-                            </div>
+                        <div className='teamposition'>
+                            {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
+                            CEO <br/> Operations
+                        </div>
+                        <div className='twitterIcon'>
+                            {/* <div className='twitter'>
+                                <FiTwitter size={20} color='#fff' />
+                            </div> */}
+                        
                         </div>
                     </div>
-                    <div className="team-member">
-                        <div className="teamImg">
-                            <img src={Jade}/>
-                        </div>
-                        <div className='teamInfor'>
-                            <div className='mytitle'>
-                                Jade
-                            </div>
-                            <div className='teamposition'>
-                                {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
-                                CTO <br/>Technology & Blockchain
-                            </div>
-                            <div className='twitterIcon'>
-                                {/* <div className='twitter'>
-                                    <FiTwitter size={20} color='#fff' />
-                                </div> */}
-                               
-                            </div>
-                        </div>
-                    </div>
-                    <div className="team-member">
-                        <div className="teamImg">
-                            <img src={KOby}/>
-                        </div>
-                        <div className='teamInfor'>
-                            <div className='mytitle'>
-                                Koby
-                            </div>
-                            <div className='teamposition'>
-                                {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
-                                Software Engineer<br/>Smart Contract/Developer
-
-                            </div>
-                            <div className='twitterIcon'>
-                                {/* <div className='twitter'>
-                                    <FiTwitter size={20} color='#fff' />
-                                </div> */}
-                               
-                            </div>
-                        </div>
-                    </div>
-                    <div className="team-member">
-                    <div className="teamImg">
-                            <img src={Andrea}/>
-                        </div>
-                        <div className='teamInfor'>
-                            <div className='mytitle'>
-                                Andrea
-                            </div>
-                            <div className='teamposition'>
-                                {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
-                                Architect/Artist <br/>Designer
-
-                            </div>
-                            <div className='twitterIcon'>
-                                {/* <div className='twitter'>
-                                    <FiTwitter size={20} color='#fff' />
-                                </div> */}
-                               
-                            </div>
-                        </div>
-                    </div>
-                   
-
                 </div>
+                <div className="team-member">
+                <div className="teamImg">
+                        <img src={Karim}/>
+                    </div>
+                    <div className='teamInfor'>
+                        <div className='mytitle'>
+                            Karim
+                        </div>
+                        <div className='teamposition'>
+                            {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
+                            CMO <br/> Marketing
+                        </div>
+                        <div className='twitterIcon'>
+                            {/* <div className='twitter'>
+                                <FiTwitter size={20} color='#fff' />
+                            </div> */}
+                        
+                        </div>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <div className="teamImg">
+                        <img src={Jade}/>
+                    </div>
+                    <div className='teamInfor'>
+                        <div className='mytitle'>
+                            Jade
+                        </div>
+                        <div className='teamposition'>
+                            {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
+                            CTO <br/>Technology & Blockchain
+                        </div>
+                        <div className='twitterIcon'>
+                            {/* <div className='twitter'>
+                                <FiTwitter size={20} color='#fff' />
+                            </div> */}
+                        
+                        </div>
+                    </div>
+                </div>
+                <div className="team-member">
+                    <div className="teamImg">
+                        <img src={KOby}/>
+                    </div>
+                    <div className='teamInfor'>
+                        <div className='mytitle'>
+                            Koby
+                        </div>
+                        <div className='teamposition'>
+                            {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
+                            Software Engineer<br/>Smart Contract/Developer
+
+                        </div>
+                        <div className='twitterIcon'>
+                            {/* <div className='twitter'>
+                                <FiTwitter size={20} color='#fff' />
+                            </div> */}
+                        
+                        </div>
+                    </div>
+                </div>
+                <div className="team-member">
+                <div className="teamImg">
+                        <img src={Andrea}/>
+                    </div>
+                    <div className='teamInfor'>
+                        <div className='mytitle'>
+                            Andrea
+                        </div>
+                        <div className='teamposition'>
+                            {/* Co-Founder of PP. Co-Creative Director and Operations Manager. NFT Collector and Computer Science Student. */}
+                            Architect/Artist <br/>Designer
+
+                        </div>
+                        <div className='twitterIcon'>
+                            {/* <div className='twitter'>
+                                <FiTwitter size={20} color='#fff' />
+                            </div> */}
+                        
+                        </div>
+                    </div>
+                </div>
+            
+
+            </div>
+                
                 
 
 
-              </div>
+        </div>
                
                
                
